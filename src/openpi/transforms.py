fix(training): preserve padded policy dimensions during normalization

src/openpi/transforms.py
@@ -135,13 +135,21 @@ class Normalize(DataTransformFn):
         )
 
     def _normalize(self, x, stats: NormStats):
-        mean, std = stats.mean[..., : x.shape[-1]], stats.std[..., : x.shape[-1]]
+        mean, std = stats.mean, stats.std
+        if (dim := mean.shape[-1]) < x.shape[-1]:
+            normalized = (x[..., :dim] - mean) / (std + 1e-6)
+            return np.concatenate([normalized, x[..., dim:]], axis=-1)
+        mean, std = mean[..., : x.shape[-1]], std[..., : x.shape[-1]]
         return (x - mean) / (std + 1e-6)
 
     def _normalize_quantile(self, x, stats: NormStats):
         assert stats.q01 is not None
         assert stats.q99 is not None
-        q01, q99 = stats.q01[..., : x.shape[-1]], stats.q99[..., : x.shape[-1]]
+        q01, q99 = stats.q01, stats.q99
+        if (dim := q01.shape[-1]) < x.shape[-1]:
+            normalized = (x[..., :dim] - q01) / (q99 - q01 + 1e-6) * 2.0 - 1.0
+            return np.concatenate([normalized, x[..., dim:]], axis=-1)
+        q01, q99 = q01[..., : x.shape[-1]], q99[..., : x.shape[-1]]
         return (x - q01) / (q99 - q01 + 1e-6) * 2.0 - 1.0
 
 